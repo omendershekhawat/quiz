14beb4f5b0cdb4dbc3fdc4732d89b8322da7bc5b
unknown <omendershekhawat1@gmail.com>
2023-08-16 12:47:07 +0530

    first

---
+++ quiz.js
@@ -0,0 +1,63 @@
+const ques=document.querySelector(".ques")
+const radios = document.querySelectorAll(".option")
+const labels = document.querySelectorAll("label")
+
+const questions = [
+    {
+      q: "2+2",
+      a: 4,
+      options: [5, 4, 3, 2],
+    },
+    {
+      q: "2*2*0",
+      a: 0,
+      options: [22, 4, 0, 2],
+    },
+    {
+      q: "3+3-3",
+      a: 3,
+      options: [9, 3, 33, 333],
+    },
+    {
+      q: "4*4/2",
+      a: 8,
+      options: [8, 16, 4, 44],
+    },
+  ];
+  let questionNumber = 1;
+  ques.innerHTML=questions[0].q
+  changeSelection(); 
+
+  let Inter = setInterval(()=>{
+  if( questionNumber === questions.length){
+    clearInterval(Inter)
+  // changeSelection()
+
+  }
+   else{
+  ques.innerHTML=questions[questionNumber].q
+  questionNumber++
+  changeSelection()
+  }
+  },1000)
+
+   
+  
+
+  function changeSelection (){
+    for(i=0; i<questions.length; i++){
+        radios[i].values = questions[questionNumber-1].options[i]
+    }
+    for(i=0; i<questions.length; i++){
+        labels[i].innerHTML = questions[questionNumber-1].options[i]
+    }
+
+  }
+  removeSelected()
+
+  function removeSelected(){
+for(i=0; i<questionNumber.length; i++){
+    if (radios[i].checked === true ) radios[i] .checked = false;
+
+}
+  }
